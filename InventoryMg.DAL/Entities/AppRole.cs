1eca92597d08fd60ab2d2ab8cea1342e4180136c
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-04-22 14:24:55 +0100

    seed data

--- InventoryMg.DAL/Entities/AppRole.cs
+++ InventoryMg.DAL/Entities/AppRole.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,14 @@ namespace InventoryMg.DAL.Entities
 {
     public class AppRole : IdentityRole
     {
+        public AppRole(): base()
+        {
+
+        }
+        public AppRole(string rolename): base(rolename)
+        {
+
+        }
+
     }
 }
---
+++ InventoryMg.DAL/Seed/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using InventoryMg.DAL.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InventoryMg.DAL.Seed
+{
+    public class SeedData
+    {
+        public static async Task EnsurePopulatedAsync(IApplicationBuilder app)
+        {
+            ApplicationDbContext appDbContext = app.ApplicationServices.CreateScope().ServiceProvider
+                .GetRequiredService<ApplicationDbContext>();
+
+            using var scope = app.ApplicationServices.CreateScope();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<AppRole>>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserProfile>>();
+
+            if (!await roleManager.RoleExistsAsync("admin"))
+            {
+                await roleManager.CreateAsync(new AppRole("admin"));
+            }
+
+            if (!await appDbContext.Users.AnyAsync())
+            {
+                var result = await userManager.CreateAsync(AddAdmin(), "AdminPass12345@");
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(AddAdmin(), "admin");
+                }
+                // await appDbContext.Users.AddAsync(AddAdmin());
+                await appDbContext.SaveChangesAsync();
+            }
+        }
+
+        private static UserProfile AddAdmin()
+        {
+            return new UserProfile()
+            {
+                FullName = "Admin One",
+                Email = "admin@domain.com",
+                Phone = "2233445566"
+            };
+        }
+
+
+
+
+    }
+}
